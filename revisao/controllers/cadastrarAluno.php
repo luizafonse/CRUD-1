d0eefbbcf7ad21ebaac3f384237c4ac4e8f474b5
Afonso Luiz <124940177+luizafonse@users.noreply.github.com>
2023-03-20 21:59:12 -0300

    Delete revisao directory

--- revisao/controllers/cadastrarAluno.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../models/conexao.php");
-
-mysqli_query($conexao, "INSERT INTO alunos (nome, cidade, sexo) VALUES ('".$_POST["alunoNome"]."', '".$_POST["alunoCidade"]."', '".$_POST["alunoSexo"]."')");
-
-header("location:../")
-?>
--- revisao/controllers/deletarAluno.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../models/conexao.php");
-
-
-mysqli_query($conexao, "DELETE FROM alunos WHERE codigo = ".$_GET["ida"]);
-
-header("location:../")
-?>
